4bd063851a7e601faa24eee484c8a4677797786d
oguz <oguz@oguz-MacBook-Pro.local>
2026-03-29 18:30:28 +0300

    auto: 2026-03-29 18:30

--- biprova/app/dashboard/(shell)/createProject/page.tsx
+++ biprova/app/dashboard/(shell)/createProject/page.tsx
@@ -1,19 +1,26 @@
+import { Suspense } from "react";
 import { CreateProjectTopbar } from "./_components/create-project-topbar";
 import { CreateProjectLeftCol } from "./_components/create-project-left-col";
 import { getCategories, getCities, getSkills } from "@/features/projects/actions";
 
-export default async function CreateProjectPage() {
+async function CreateProjectContent() {
   const [categories, cities, skills] = await Promise.all([
     getCategories(),
     getCities(),
     getSkills(),
   ]);
 
+  return <CreateProjectLeftCol categories={categories} cities={cities} skills={skills} />;
+}
+
+export default function CreateProjectPage() {
   return (
     <div>
       <CreateProjectTopbar />
       <div className="p-6 md:p-8">
-        <CreateProjectLeftCol categories={categories} cities={cities} skills={skills} />
+        <Suspense>
+          <CreateProjectContent />
+        </Suspense>
       </div>
     </div>
   );
