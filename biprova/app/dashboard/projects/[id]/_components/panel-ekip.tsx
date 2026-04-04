dd3eaaac93f183d9db20a306301778dbdb75cf0d
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-04 23:57:33 +0300

    auto: 2026-04-04 23:57

--- biprova/app/dashboard/projects/[id]/_components/panel-ekip.tsx
+++ biprova/app/dashboard/projects/[id]/_components/panel-ekip.tsx
@@ -49,12 +49,13 @@ export function PanelEkip({ project }: Props) {
         />
       )}
 
-      {!viewer.is_creator && !viewer.is_team_leader && (
-        <LeaveProjectSection projectId={project.id} />
-      )}
-
-      {viewer.is_creator && (
-        <TransferCreatorSection projectId={project.id} members={project.members} viewerId={viewer.id} />
+      {!viewer.is_team_leader && (
+        <LeaveProjectSection
+          projectId={project.id}
+          isCreator={viewer.is_creator}
+          members={project.members}
+          viewerId={viewer.id}
+        />
       )}
     </div>
   );
