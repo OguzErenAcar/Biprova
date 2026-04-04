2f9cd38ec2092e5ea580a8f2e09d1ddac01d8276
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-05 00:06:56 +0300

    auto: 2026-04-05 00:06

--- biprova/db/policies.sql
+++ biprova/db/policies.sql
@@ -102,7 +102,7 @@ create policy "user_skills_manage" on user_skills for all    using (user_id = au
 -- project_roles
 create policy "project_roles_read"   on project_roles for select using (true);
 create policy "project_roles_manage" on project_roles for all
-    using (is_project_creator(project_id));
+    using (is_project_leader(project_id));
 
 -- project_role_skills
 create policy "role_skills_read"   on project_role_skills for select using (true);
