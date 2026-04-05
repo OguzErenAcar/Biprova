0dc6779b0708c1625d912d3ebdb369b7b32bf38b
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-05 18:43:54 +0300

    auto: 2026-04-05 18:43

--- biprova/app/dashboard/(shell)/profile/[id]/page.tsx
+++ biprova/app/dashboard/(shell)/profile/[id]/page.tsx
@@ -24,6 +24,8 @@ async function ProfileContent({ id }: { id: string }) {
     getUserTeams(user.id),
   ]);
 
+  console.log('DEBUG visibility', { isOwner, p: user.projects_public, t: user.teams_public, a: user.applications_public });
+
   return (
     <div className="">
       <ProfileHero user={user} isOwner={isOwner} />
