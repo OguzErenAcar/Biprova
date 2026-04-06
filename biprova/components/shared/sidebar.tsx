4444c15bab9a5db8887f806d2a023e388df0d0af
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-06 17:04:07 +0300

    auto: 2026-04-06 17:04

--- biprova/components/shared/sidebar.tsx
+++ biprova/components/shared/sidebar.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-import { SidebarAccountActions } from "@/components/shared/sidebar-account-actions";
 
 interface SidebarUser {
   name: string;
