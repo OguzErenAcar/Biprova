8414af82f5aef61fba1a6e41506eeb2396d60a16
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-06 16:59:51 +0300

    auto: 2026-04-06 16:59

---
+++ biprova/app/dashboard/(shell)/notifications/page.tsx
@@ -0,0 +1,7 @@
+export default function NotificationsPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="font-nunito font-black text-[1.3rem] text-slate-900">Bildirimler</h1>
+    </div>
+  )
+}
