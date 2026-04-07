4c70ad88db69b66b3927b0a36b45021b368787c4
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-07 13:00:48 +0300

    auto: 2026-04-07 13:00

--- biprova/app/dashboard/projects/[id]/_components/panel-admin.tsx
+++ biprova/app/dashboard/projects/[id]/_components/panel-admin.tsx
@@ -10,6 +10,7 @@ import { inviteToProject, removeFromProject } from '@/features/projects/actions'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Dialog,
   DialogContent,
