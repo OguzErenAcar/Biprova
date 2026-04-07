381a3192ebca217707f78a32b446f1218018673e
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-08 02:56:54 +0300

    auto: 2026-04-08 02:56

--- biprova/components/shared/home-topbar.tsx
+++ biprova/components/shared/home-topbar.tsx
@@ -9,7 +9,7 @@ export function HomeTopbar() {
           {/* Logo */}
       <Link
         href="/dashboard"
-        className="font-nunito font-black text-[1.3rem] text-blue-600 px-2  no-underline"
+        className="font-nunito font-black text-h2 text-blue-600 px-2  no-underline"
       >
         Bi<span className="text-slate-900">prova</span>
       </Link>
