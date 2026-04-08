1f5f8f6f698dbc88d7373d46794de1abb9804deb
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-08 12:47:29 +0300

    auto: 2026-04-08 12:47

--- biprova/app/layout.tsx
+++ biprova/app/layout.tsx
@@ -1,20 +1,7 @@
 import type { Metadata } from "next";
-import { Nunito, Plus_Jakarta_Sans } from "next/font/google";
-import "./globals.css";
+ import "./globals.css";
 
-const nunito = Nunito({
-  variable: "--font-nunito",
-  display: "swap",
-  subsets: ["latin"],
-  weight: ["400", "600", "700", "800", "900"],
-});
-
-const jakarta = Plus_Jakarta_Sans({
-  variable: "--font-jakarta",
-  display: "swap",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
+ 
 
 export const metadata: Metadata = {
   title: "biprova",
@@ -29,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <body
-        className={`${nunito.variable} ${jakarta.variable} antialiased`}
+        className={`  antialiased`}
       >
         {children}
       </body>
