119c26d953348f9b86180bcd7dd8e70a4b46c6f3
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-10 23:57:59 +0300

    auto: 2026-04-10 23:57

--- biprova/app/dashboard/layout.tsx
+++ biprova/app/dashboard/layout.tsx
@@ -27,7 +27,7 @@ export default function DashboardLayout({
         </DashboardGrid>
       </div>
       <TabBar />
-      <Toaster position="bottom-center" />
+      <Toaster position="bottom-center" offset={120} />
     </div>
   );
 }
