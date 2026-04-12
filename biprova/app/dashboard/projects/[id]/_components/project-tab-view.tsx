456b3bc1a6759528f30d1d0ba190ec8d54ba4bab
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-12 18:16:25 +0300

    auto: 2026-04-12 18:16

--- biprova/app/dashboard/projects/[id]/_components/project-tab-view.tsx
+++ biprova/app/dashboard/projects/[id]/_components/project-tab-view.tsx
@@ -150,7 +150,7 @@ export function ProjectTabView({ project }: Props) {
       </div>
 
       {/* Content */}
-      <div ref={contentRef} className="p-4 sm:p-6">
+      <div ref={contentRef} className="p-4 sm:p-6 pb-[calc(1rem+56px)] lg:pb-6">
         {activeTab === 'genel' && (
           <PanelGenel
             project={project}
