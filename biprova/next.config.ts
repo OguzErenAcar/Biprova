70b010b99a2921f0c2cf42db2243339e52e56d4c
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-13 17:10:10 +0300

    2026-04-13 17:10

--- biprova/next.config.ts
+++ biprova/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.110'],
+  experimental: {
+    turbo: {
+      root: __dirname,
+    },
+  },
 };
 
 export default nextConfig;
