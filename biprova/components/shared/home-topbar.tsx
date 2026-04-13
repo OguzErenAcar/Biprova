952d5d5c0dfe2d29784d04427212289314356caf
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-13 17:36:59 +0300

    2026-04-13 17:36

--- biprova/components/shared/home-topbar.tsx
+++ biprova/components/shared/home-topbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, ElementType } from 'react';
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { animate, createTimeline, splitText, stagger } from 'animejs';
