ac2e9bcfc78aa9145a56ef0f2905edd6a2cbbf33
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-14 14:21:12 +0300

    2026-04-14 14:21

--- biprova/components/shared/content-header.tsx
+++ biprova/components/shared/content-header.tsx
@@ -49,7 +49,7 @@ export function ContentHeader({ title, children }: ContentHeaderProps) {
 
   return (
     <div className="relative sticky top-0 z-40 backdrop-blur-[12px] flex items-center gap-4">
-      <div className="flex items-between w-full justify-between my-2">
+      <div className="flex items-between w-full justify-between my-2 px-3 md:px-0">
         <h1 ref={titleRef} className="dashheader my-auto">{title}</h1>
         {children && (
           <div className="flex items-center">{children}</div>
