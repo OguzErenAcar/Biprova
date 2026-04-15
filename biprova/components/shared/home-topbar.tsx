042ad7b57bad89103eff8ab136af595c09a1f786
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-15 19:27:46 +0300

    2026-04-15 19:27

--- biprova/components/shared/home-topbar.tsx
+++ biprova/components/shared/home-topbar.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState, ElementType } from 'react';
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { notify } from "@/lib/notify";
 import { getUserLocation } from "@/lib/location";
 import { animate, createTimeline, splitText, stagger } from 'animejs';
