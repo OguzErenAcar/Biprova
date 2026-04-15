45db21cd4bb042daca6fee37d8d575a818f5fb78
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-16 02:56:31 +0300

    2026-04-16 02:56

--- biprova/app/dashboard/projects/[id]/_components/panel-ekip.tsx
+++ biprova/app/dashboard/projects/[id]/_components/panel-ekip.tsx
@@ -57,7 +57,7 @@ function MembersSection({ members }: { members: ProjectDetail['members'] }) {
               <UserAvatar
                 avatarUrl={member.avatar_url}
                 initials={getInitials(member.name)}
-                badge={member.badge}
+                badge={member.badge_url}
                 size={32}
                 className="text-[0.72rem]"
               />
