79ecb479492146f19f0001d49013c646dd3c37e7
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-15 19:57:38 +0300

    2026-04-15 19:57

--- biprova/components/shared/sidebar-loader.tsx
+++ biprova/components/shared/sidebar-loader.tsx
@@ -1,13 +1,7 @@
-import { cookies } from "next/headers";
 import { createClient } from "@/lib/supabase/server";
 import { Sidebar } from "@/components/shared/sidebar";
-import { parseLocationCookie } from "@/features/projects/location-utils";
 
 export async function SidebarLoader() {
-  const cookieStore = await cookies();
-  const locationCookie = cookieStore.get("location-filter")?.value;
-  const locationOn = parseLocationCookie(locationCookie) !== null;
-
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
 
@@ -29,5 +23,5 @@ export async function SidebarLoader() {
     }
   }
 
-  return <Sidebar projects={projects} locationOn={locationOn} />;
+  return <Sidebar projects={projects} />;
 }
