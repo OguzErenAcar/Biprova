68251cb0637379888aab915c10ea111f1a846709
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-17 00:30:10 +0300

    2026-04-17 00:30

--- biprova/app/dashboard/(shell)/layout.tsx
+++ biprova/app/dashboard/(shell)/layout.tsx
@@ -1,13 +1,13 @@
- 
+import { PullToRefresh } from "@/components/shared/pull-to-refresh";
+
 export default function ShellLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-
+      <PullToRefresh />
       <div id="dashboard-shell" className="">
         <div id="dashboard-main">
           {children}
         </div>
-
       </div>
     </>
   );
