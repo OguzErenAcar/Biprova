1d066d4e8bf5448bf70727e13ab70b6a3aaf5700
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-20 00:16:45 +0300

    2026-04-20 00:16

--- biprova/app/layout.tsx
+++ biprova/app/layout.tsx
@@ -1,23 +1,23 @@
 import type { Metadata } from "next";
-import { headers } from "next/headers";
-import "./globals.css";
+ import "./globals.css";
+
+
 
 export const metadata: Metadata = {
   title: "biprova",
   description: "Bir projem var.",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const headersList = await headers();
-  const nonce = headersList.get('x-nonce') ?? '';
-
   return (
     <html lang="tr">
-      <body className="antialiased" nonce={nonce}>
+      <body
+        className={`  antialiased`}
+      >
         {children}
       </body>
     </html>
