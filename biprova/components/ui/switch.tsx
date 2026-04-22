15644d2f8b50aeb3f7cee435f502ccc049a50b19
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-22 20:14:59 +0300

    auto: 2026-04-22 20:14

--- biprova/components/ui/switch.tsx
+++ biprova/components/ui/switch.tsx
@@ -25,7 +25,7 @@ function Switch({
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "pointer-events-none block rounded-full bg-white ring-0 shadow-md transition-transform group-data-[size=default]/switch:size-4 group-data-[size=sm]/switch:size-3 data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0 dark:data-[state=checked]:bg-primary-foreground dark:data-[state=unchecked]:bg-white"
+          "pointer-events-none block rounded-full bg-black ring-0 transition-transform group-data-[size=default]/switch:size-4 group-data-[size=sm]/switch:size-3 data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0 dark:data-[state=checked]:bg-primary-foreground dark:data-[state=unchecked]:bg-white"
         )}
       />
     </SwitchPrimitive.Root>
