54e20bc08dd7c7344f6349a0a0d0d20edf38a9fc
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-22 18:29:12 +0300

    2026-04-22 18:29

--- biprova/app/dashboard/layout.tsx
+++ biprova/app/dashboard/layout.tsx
@@ -24,7 +24,7 @@ export default function DashboardLayout({
               </Suspense>
             }
           >
-            <SplashWrapper>{children}</SplashWrapper>
+            {children}
           </DashboardGrid>
         </div>
         <TabBar />
