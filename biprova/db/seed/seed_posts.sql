71f3d6ca69ec83bd96359998572003f1b4f26f81
oguz <oguz@oguz-MacBook-Pro.local>
2026-04-26 02:18:29 +0300

    auto: 2026-04-26 02:18

--- biprova/db/seed/seed_posts.sql
+++ biprova/db/seed/seed_posts.sql
@@ -12,7 +12,7 @@
 
 insert into team_posts (id, team_id, author_id, project_id, content, image_url, image_urls, like_count, created_at) values
 (
-    'p0000000-0000-0000-0000-000000000001',
+    'ab000000-0000-0000-0000-000000000001',
     (select id from teams where project_id = 'd0000000-0000-0000-0000-000000000004' limit 1),
     'a0000000-0000-0000-0000-000000000004',
     'd0000000-0000-0000-0000-000000000004',
