a49a685b90589530f2a48d8f36f91f67fdc943fe
oguz <oguz@oguz-MacBook-Pro.local>
2026-05-10 21:39:28 +0300

    2026-05-10 21:39

--- biprova/app/dashboard/(shell)/posts/layout.tsx
+++ biprova/app/dashboard/(shell)/posts/layout.tsx
@@ -1,9 +1,6 @@
-import { LastActiveUsers } from "@/components/shared/last-active-users";
-
 export default function ShellLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <LastActiveUsers />
       {children}
     </>
   );
