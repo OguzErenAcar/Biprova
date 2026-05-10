6f15df8e3395fa24725c9f950431c0b2aab947c1
oguz <oguz@oguz-MacBook-Pro.local>
2026-05-10 21:39:12 +0300

    2026-05-10 21:39

--- biprova/components/shared/sidebar-loader.tsx
+++ biprova/components/shared/sidebar-loader.tsx
@@ -46,5 +46,5 @@ export async function SidebarLoader() {
       .eq("id", user.id);
   }
 
-  return <Sidebar projects={projects} showInfoDialog={showInfoDialog} />;
+  return <Sidebar projects={projects} showInfoDialog={showInfoDialog} lastActiveUsers={<LastActiveUsers />} />;
 }
