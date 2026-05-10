a65498127c39f354d885042a25961399ac36f66b
oguz <oguz@oguz-MacBook-Pro.local>
2026-05-10 21:47:44 +0300

    2026-05-10 21:47

--- biprova/components/shared/home-topbar.tsx
+++ biprova/components/shared/home-topbar.tsx
@@ -251,6 +251,7 @@ function MobileDrawer({ open, onClose, projects, lastActiveUsers }: { open: bool
               Ayarlar
             </Link>
           </div>
+          {lastActiveUsers}
         </div>
 
         <Separator />
