68cd6876fa5c80a99e3602153fc21cb6d9149f06
oguz <oguz@oguz-MacBook-Pro.local>
2026-05-10 21:13:47 +0300

    2026-05-10 21:13

--- biprova/components/shared/last-active-users.tsx
+++ biprova/components/shared/last-active-users.tsx
@@ -1,5 +1,7 @@
 export function LastActiveUsers() {
   return (
-    <div className="last-active-users" />
+    <div className="last-active-users p-4 bg-red-100 text-red-700 font-bold text-center rounded-xl mb-4">
+      LastActiveUsers — test
+    </div>
   );
 }
